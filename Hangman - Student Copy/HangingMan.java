1a89952571a20ae5f74271d24bbbff70d85fde65
DestroERRR <55767542+DestroERRR@users.noreply.github.com>
2021-11-16 09:36:24 -0800

    Add files via upload

---
+++ Hangman - Student Copy/HangingMan.java
@@ -0,0 +1,59 @@
+public class HangingMan {
+
+    private int numWrongGuesses;
+
+    public boolean isntDead() {
+        return true;
+    }
+
+    public void show(){
+     numWrongGuesses = 1;   
+    }
+    
+    public void dieSomeMore() {
+        
+    }
+    
+    String[] HangmanImage = {"+----+\n"+
+            "|\n"+
+            "|\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|    +\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|    +-\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|\n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|   / \n"+
+            "/\\\n",
+
+            "+----+\n"+
+            "|    O\n"+
+            "|   -+-\n"+
+            "|   / \\\n"+
+            "/\\\n",
+        };
+}
---
+++ Hangman - Student Copy/Puzzle.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Puzzle
+{
+    public Puzzle(){
+        
+    }
+    
+    public boolean isUnsolved() {
+        return true;
+    }
+    
+    
+    public boolean makeGuess(String str) {
+     return false;   
+    }
+    
+    public String getWord(){
+     String str = "";
+     return str; 
+    }
+    
+    
+    public void show() {
+        
+    }
+}
